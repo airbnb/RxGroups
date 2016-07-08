69908f6b853b722b11c8739c743fb5692850480c
Felipe Lima <felipe.lima@gmail.com>
2016-07-08 14:01:27 -0700

    Adds comments

--- rxgroups-android/src/main/java/com/airbnb/rxgroups/GroupLifecycleManager.java
+++ rxgroups-android/src/main/java/com/airbnb/rxgroups/GroupLifecycleManager.java
@@ -182,6 +182,9 @@ public class GroupLifecycleManager {
     // being destroyed and are not ever coming back. However, this isn't perfect, especially
     // when using fragments in a ViewPager. We might want to allow users to explicitly destroy it
     // instead, in order to mitigate this issue.
+    // Also in some situations it seems like an Activity can be recreated even though its
+    // isFinishing() property is set to true. For this reason, we must also check
+    // isChangingConfigurations() to make sure it's safe to destroy the group.
     onDestroy(!hasSavedState
         || (activity != null && activity.isFinishing() && !activity.isChangingConfigurations()));
   }
