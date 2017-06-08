925722dfea21bddbad1d34fbb65b793d17965e66
Ben Schwab <ben.schwab@airbnb.com>
2017-06-07 17:48:27 -0700

    Prevent null tag collisions by resorting to NonResubscribingTag, if getTag returns null

--- rxgroups/src/main/java/com/airbnb/rxgroups/Utils.java
+++ rxgroups/src/main/java/com/airbnb/rxgroups/Utils.java
@@ -5,7 +5,10 @@ import rx.Observer;
 public class Utils {
   static String getObserverTag(Observer<?> observer) {
     if (observer instanceof TaggedObserver) {
-      return ((TaggedObserver) observer).getTag();
+      String definedTag = ((TaggedObserver) observer).getTag();
+      if (definedTag != null) {
+        return definedTag;
+      }
     }
     return NonResubscribableTag.create(observer);
   }
